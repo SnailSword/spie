66a1c865fb34c64f3fc36092af452128d2bf1385
hantianjiao <hantianjiao@baidu.com>
2018-08-23 11:31:45 +0800

    modify config
    Change-Id: Ief0e6ba6b4bae2968d42ff9f2d5a8cb9b4b64a18

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
     input: __dirname + '/src/main.js',
     plugins: [
         nodeResolve(),
         commonjs(),
+        uglify.uglify(),
         babel({
             exclude: 'node_modules/**',
         })
